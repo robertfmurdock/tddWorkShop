7dcc8ddab4b50e018d2e31faa48a8b11cedd67c4
Rob Murdock <rob.murdock@integral.io>
2021-06-02 07:48:03 -0400

    Cleaning up for a modern JVM

--- integrationKata/src/test/java/IntegrationTest.java
+++ integrationKata/src/test/java/IntegrationTest.java
@@ -1,6 +1,6 @@
-import static org.junit.Assert.*;
+import org.junit.jupiter.api.Test;
 
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class IntegrationTest {
 
